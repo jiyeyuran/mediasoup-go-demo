4855581aecd51053fa8817feb70c5dbe32ee9de2
武新飞 <xinfei.wu@qq.com>
2021-03-07 11:00:13 +0800

    feat(log): control log level via environment DEBUG

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
+	github.com/gobwas/glob v0.2.3
 	github.com/gorilla/websocket v1.4.2
 	github.com/jiyeyuran/go-eventemitter v1.1.2
 	github.com/jiyeyuran/go-protoo v1.0.1
--- server/logger.go
+++ server/logger.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"os"
+	"strings"
 	"time"
 
+	"github.com/gobwas/glob"
 	"github.com/rs/zerolog"
 )
 
@@ -12,5 +14,34 @@ var NewLogger = func(name string) zerolog.Logger {
 		Out:        os.Stderr,
 		TimeFormat: time.RFC3339,
 	}
-	return zerolog.New(w).With().Timestamp().Str(zerolog.CallerFieldName, name).Logger()
+
+	shouldDebug := false
+
+	if debug := os.Getenv("DEBUG"); len(debug) > 0 {
+		for _, part := range strings.Split(debug, ",") {
+			part := strings.TrimSpace(part)
+			if len(part) == 0 {
+				continue
+			}
+
+			shouldMatch := true
+			if part[0] == '-' {
+				shouldMatch = false
+				part = part[1:]
+			}
+			if g := glob.MustCompile(part); g.Match(name) {
+				shouldDebug = shouldMatch
+			}
+		}
+	} else {
+		shouldDebug = true
+	}
+
+	level := zerolog.InfoLevel
+
+	if shouldDebug {
+		level = zerolog.DebugLevel
+	}
+
+	return zerolog.New(w).With().Timestamp().Str(zerolog.CallerFieldName, name).Logger().Level(level)
 }
